3e7afeeaa1e846412e594eec2f33607c969a880f
Amertaaa <syahrulkhaliq58@gmail.com>
2023-09-27 10:13:39 +0700

    tugas4

--- main/models.py
+++ main/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     date_added = models.DateField(auto_now_add=True)
     price  = models.IntegerField()
     amount = models.IntegerField()
     description = models.TextField(max_length=255)
+    
 # Create your models here.
+# class Employee(models.Model): 
+#     name = models.CharField(max_length=255)
+#     age  = models.IntegerField()
+#     hobby = models.TextField(max_length=255)
--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
-
-
+from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id,update_amount, delete_item
+from main.views import register
+from main.views import login_user
+from main.views import logout_user
 app_name = 'main'
 
 urlpatterns = [
@@ -11,4 +12,9 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('update-amount/<str:item_name>/<str:action>/', update_amount, name='update_amount'),
+    path('delete-item/<str:item_name>/', delete_item, name='delete_item'),
 ]
